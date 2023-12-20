e950dd051f0dd91eefe21ffbe25b0954e0a32e0d
makvis <m0303vk@ya.ru>
2023-12-21 00:21:12 +0300

    f commit

---
+++ src/components/layout/HeaderComponent.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+@Component({})
+export default class HeaderComponent extends Vue {}
+</script>
+<template>
+  <el-header height="auto">
+    <el-row type="flex" class="row-bg" justify="space-between">
+      <el-col class="header-logo" :span="12">
+        <a href="#">  <!--все картинки в папке assets*/ -->
+            <img src="@/assets/img/log.jpg" alt="logo" />
+        </a>
+      <el-col :span="12"
+        ><div>
+          <el-menu :default-active="activeIndex" mode="horizontal">
+            <el-menu-item index="1">Processing Center</el-menu-item>
+            <el-menu-item index="3" disabled>Info</el-menu-item>
+            <el-menu-item index="4"></el-menu-item>
+          </el-menu></div
+      ></el-col>
+    </el-row>
+  </el-header>
+</template>
+<style lang="scss" scroped></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo192.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
